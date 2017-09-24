d2f3810631a6f72268e24ebb22a428a10a083f56
Woodsmike <Woodsmike@users.noreply.github.com>
2017-09-24 18:29:15 -0400

    Initial Commit

---
+++ CSharp_RepositoryPattern/Models/CSharp_RepositoryPatternContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CSharp_RepositoryPattern.Models
+{
+    public class CSharp_RepositoryPatternContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public CSharp_RepositoryPatternContext() : base("name=CSharp_RepositoryPatternContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<CSharp_RepositoryPattern.Models.Student> Students { get; set; }
+
+        public System.Data.Entity.DbSet<CSharp_RepositoryPattern.Models.Faculty> Faculties { get; set; }
+    }
+}
---
+++ CSharp_RepositoryPattern/Models/Faculty.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSharp_RepositoryPattern.Models
+{
+    public class Faculty
+    {
+        public int FacultyID { get; set; }
+        public string Name { get; set; }
+    }
+}
